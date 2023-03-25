f265b4836f70f2df729aa477e425964fe441cd7a
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-03-25 15:09:05 +0100

    comments has been added

--- LiveFootballScoreboard.js
+++ LiveFootballScoreboard.js
@@ -4,7 +4,9 @@ const LiveFootballScoreboard = () => {
   // This games array holds all the games currently in progress!
   const games = [];
 
+  // a function to start a new game and add it to the scoreboard
   function startNewGame(homeTeam, awayTeam) {
+    // new game object with the current time, home team, away team, and initial scores of 0-0
     const game = {
       startTime: Date.now(),
       homeTeam,
@@ -12,11 +14,15 @@ const LiveFootballScoreboard = () => {
       homeScore: 0,
       awayScore: 0,
     };
+    // Adding the game object to the games array
     games.push(game);
+    // returning the game object
     return game;
   }
 
+  // a function to update the score of a given game
   function updateScore(game, homeScore, awayScore) {
+    // updating the home score and away score of the game object
     game.homeScore = homeScore;
     game.awayScore = awayScore;
   }
